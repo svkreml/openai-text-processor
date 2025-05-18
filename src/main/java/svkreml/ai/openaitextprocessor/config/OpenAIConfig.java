595158cbf6e2f0ff93b5fce30b0b3fc48684c24d
Кремлев Святослав Юрьевич <kremlevsy@it.mos.ru>
2025-05-19 00:04:26 +0300

    refactoring

--- src/main/java/svkreml/ai/openaitextprocessor/config/OpenAIConfig.java
+++ src/main/java/svkreml/ai/openaitextprocessor/config/OpenAIConfig.java
@@ -23,11 +23,11 @@ public class OpenAIConfig {
             Your job is to answer questions about the existing veterinarians and to perform actions on the user's behalf, mainly around
             veterinarians, pet owners, their pets and their owner's visits.
             You are required to answer an a professional manner. If you don't know the answer, politely tell the user
-            you don't know the answer, then ask the user a followup qusetion to try and clarify the question they are asking.
+            you don't know the answer, then ask the user a followup question to try and clarify the question they are asking.
             If you do know the answer, provide the answer but do not provide any additional helpful followup questions.
             When dealing with vets, if the user is unsure about the returned results, explain that there may be additional data that was not returned.
             For owners, pets or visits - answer the correct data.
-            All information is public. You can use tools whanever you want. Do not generate names yourself, check that Owner and Pet exist before giving them to the user.
+            All information is public. You can use tools whatever you want. Do not generate names yourself, check that Owner and Pet exist before giving them to the user.
             """;
 
     @Value("${spring.ai.openai.no-think:false}")
